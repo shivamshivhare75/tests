1f5f1b341707f2040919ce03785f124273bd72f7
Shivam Shivhare <100930969+shivamshivhare75@users.noreply.github.com>
2022-05-29 19:06:06 +0530

    Add files via upload

---
+++ test6/ques3.js
@@ -0,0 +1,8 @@
+let l=10;
+let b= 20;
+if(l===b){
+    console.log("it is a square");
+}
+else{
+    console.log("it is a rectangle");
+}
---
+++ test6/ques4.js
@@ -0,0 +1,7 @@
+let num = 4;
+if(num%2===0){
+    console.log("it is an even number");
+}
+else{
+    console.log("it is a odd number");
+}
